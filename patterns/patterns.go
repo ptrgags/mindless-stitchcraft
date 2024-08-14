d74f8f4507fe6918304aff915f0bbf28ada5d512
Peter Gagliardi <ptrgags@gmail.com>
2024-08-14 17:07:30 -0400

    Start working on pattern generation

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -6,13 +6,21 @@ import (
 	"strings"
 )
 
+func validateStitches(stitches string) error {
+	regex := regexp.MustCompile("^(v|-)*$")
+	if stitches == "" || !regex.MatchString(stitches) {
+		return errors.New("invalid stitches string. It must be a string of at least 1 character composed of knits ('v') and purls ('-')")
+	}
+
+	return nil
+}
+
 // Take a string of stiches (either 'v' for knit or '-' for purl)
 // and swap the knits and purls. This is one part of what happens when
 // you flip the fabric over when knitting. This returns a new string
 func SwapKnitsAndPurls(stitches string) (string, error) {
-	regex := regexp.MustCompile("^(v|-)*$")
-	if !regex.MatchString(stitches) {
-		return "", errors.New("invalid stitches string. Stitches are composed of knits ('v') and purls ('-')")
+	if err := validateStitches(stitches); err != nil {
+		return "", err
 	}
 
 	return strings.Map(func(r rune) rune {
@@ -24,4 +32,44 @@ func SwapKnitsAndPurls(stitches string) (string, error) {
 
 		return r
 	}, stitches), nil
+}
+
+func generatePatternShort(stitches string, width int) ([]string, error) {
+	return []string{}, nil
+}
+
+func generatePatternLong(stitches string, width int) ([]string, error) {
+	return []string{}, nil
+}
+
+// Take a pattern of knits and purls e.g. repeat it
+// over and over until the pattern repeats for the given number of stitches
+// wide. E.g. for input "v--", 5 we have:
+//
+// v--v-
+// -v--v
+// --v--
+//
+// This method assumes all stitches are on the right side of the fabric,
+// and the first stitch is the top left, other functions will convert this
+// to a usable knitting chart
+func GeneratePattern(stitches string, width int) ([]string, error) {
+	if err := validateStitches(stitches); err != nil {
+		return []string{}, err
+	}
+
+	n := len(stitches)
+
+	// Handle simple cases
+	if n == width {
+		return []string{stitches}, nil
+	}
+
+	// The logic is a little different depending on whether the stitches
+	// fit within the width or the other way around.
+	if n < width {
+		return generatePatternShort(stitches, width)
+	}
+
+	return generatePatternLong(stitches, width)
 }
--- patterns/patterns_test.go
+++ patterns/patterns_test.go
@@ -6,20 +6,20 @@ import (
 )
 
 func TestSwapKnitsAndPurls(t *testing.T) {
-
-	t.Run("empty input is unchanged", func(t *testing.T) {
-		swapped, err := SwapKnitsAndPurls("")
-		if swapped != "" || err != nil {
-			t.Errorf("Expected ('', nil), got (%v, %v)", swapped, err)
-		}
-	})
-
 	t.Run("invalid characters result in an error", func(t *testing.T) {
-		// Knits are represented as v in this program...
-		badChars := "🧶vvv"
-		swapped, err := SwapKnitsAndPurls(badChars)
-		if err == nil || !strings.Contains(err.Error(), "invalid stitches") {
-			t.Errorf("Expected invalid stitches error, got (%v, %v)", swapped, err)
+		cases := []struct {
+			label string
+			input string
+		}{
+			{"empty", ""},
+			{"bad character", "-v🧶"},
+		}
+
+		for _, tc := range cases {
+			swapped, err := SwapKnitsAndPurls(tc.input)
+			if err == nil || !strings.Contains(err.Error(), "invalid stitches") {
+				t.Errorf("(%v) Expected invalid stitches error, got (%v, %v)", tc.label, swapped, err)
+			}
 		}
 	})
 
@@ -55,5 +55,66 @@ func TestSwapKnitsAndPurls(t *testing.T) {
 				t.Errorf("Expected (%v, nil), got (%v, %v)", tc.expected, swapped, err)
 			}
 		}
+	})
+}
+
+func TestGeneratePattern(t *testing.T) {
+	t.Run("empty stitches string gives error", func(t *testing.T) {
+		rows, err := GeneratePattern("", 5)
+		if err == nil || !strings.Contains(err.Error(), "stitches must be non-empty") {
+			t.Errorf("Expected non-empty stitches error, got (%v, %v)", rows, err)
+		}
+	})
+
+	t.Run("invalid stitches gives error", func(t *testing.T) {
+		rows, err := GeneratePattern("-🧶-", 5)
+		if err == nil || !strings.Contains(err.Error(), "invalid stitches string") {
+			t.Errorf("Expected invalid stitches error, got (%v, %v)", rows, err)
+		}
+	})
+
+	t.Run("Invalid width gives error", func(t *testing.T) {
+		widths := []int{0, -1}
+
+		for _, width := range widths {
+			rows, err := GeneratePattern("v--", width)
+			if err == nil || !strings.Contains(err.Error(), "invalid width") {
+				t.Errorf("(%v): Expected invalid width error, got (%v, %v)", width, rows, err)
+			}
+		}
+	})
+
+	t.Run("len(stitches) == width results in one row", func(t *testing.T) {
+		rows, err := GeneratePattern("v-vv--", 6)
+		if err != nil || len(rows) != 1 || rows[0] != "v-vv--" {
+			t.Errorf(`Expected (["v-vv--"], nil), got (%v, %v)`, rows, err)
+		}
+	})
+
+	t.Run("len(stitches) evenly divides width results in one row", func(t *testing.T) {
+		rows, err := GeneratePattern("v--", 6)
+		if err != nil || len(rows) != 1 || rows[0] != "v--v--" {
+			t.Errorf(`Expected (["v--v--"], nil), got (%v, %v)`, rows, err)
+		}
+	})
+
+	t.Run("len(stiches) < width, not coprime", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("len(stitches) < width, coprime", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("len(stitches) is a multiple of width", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("len(stitches) > width, not coprime", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("len(stitches) > width, not coprime", func(t *testing.T) {
+		t.Fail()
 	})
 }
