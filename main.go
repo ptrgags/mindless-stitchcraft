4ec4e41f7066674ef4b127b0587b875aea4bbe6f
Peter Gagliardi <ptrgags@gmail.com>
2024-08-18 15:04:18 -0400

    Snapshot from 2024-08-15

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/ptrgags/mindless-knitting/patterns"
+)
+
 func main() {
-	//fmt.Println(patterns.SwapKnitsAndPurls("v--v--"))
+
+	pattern := os.Args[2]
+	fabricWidth, _ := strconv.Atoi(os.Args[3])
+	fmt.Println(os.Args)
+
+	rows, _ := patterns.GeneratePrintablePattern(pattern, fabricWidth)
+	for _, row := range rows {
+		fmt.Println(row)
+	}
+
+	fmt.Println()
+	fmt.Println(pattern, fabricWidth)
 }
